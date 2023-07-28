0c73f37abecbc52f942d3c771fea74373d8e8f1e
Tuanhv <tuanhv@nhanh.vn>
2023-07-28 16:44:09 +0700

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+if ($(".banner_home").length) {
+    $(".banner_home").owlCarousel({
+        items: 1,
+        nav: false,
+        dots: false,
+        lazyLoad: true,
+        loop: true,
+        autoHeight:true
+    });
+}
+
+
+
+
+
+});
