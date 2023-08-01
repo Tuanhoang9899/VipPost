f873c269f7d115feff7d7ab1cf2bc133b6318867
Tuanhv <tuanhv@nhanh.vn>
2023-08-02 00:53:35 +0700

    update code

--- main.js
+++ main.js
@@ -29,6 +29,17 @@ if ($(".banner_home").length) {
         $(".criteria .criteria_text .icon_2 img").after($(".about_me .criteria .criteria_text .text .criteria_text_2"));
         $(".criteria .criteria_text .icon_3 img").after($(".about_me .criteria .criteria_text .text .criteria_text_3"));
     }
+    $(".header-menu-mobile .nav_bar_mobile").click(function (){
+        $(".modal_menu-mobile").addClass("show-mobile");
+        $("body").css("overflow", "hidden");
+    });
+    $(".menu-mobile").click(function(event) {
+        event.stopPropagation();
+    });
+    $(".modal_menu-mobile").click(function() {
+        $(this).removeClass("show-mobile");
+        $("body").css("overflow", "visible");
+    });
 
 
 
