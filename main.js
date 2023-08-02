1006ab708457fb1f375af6e99070fa3a7cfccea5
Tuanhv <tuanhv@nhanh.vn>
2023-08-02 17:21:00 +0700

    update code

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ if ($(".banner_home").length) {
     });
 }
 
-    if ($(".box_blog_slide").length) {
-        $(".box_blog_slide").owlCarousel({
+    if ($(".box_blog_slide_home").length) {
+        $(".box_blog_slide_home").owlCarousel({
             items: 1,
             nav: false,
             dots: true,
@@ -28,6 +28,7 @@ if ($(".banner_home").length) {
         $(".criteria .criteria_text .icon_1 img").after($(".about_me .criteria .criteria_text .text .criteria_text_1"));
         $(".criteria .criteria_text .icon_2 img").after($(".about_me .criteria .criteria_text .text .criteria_text_2"));
         $(".criteria .criteria_text .icon_3 img").after($(".about_me .criteria .criteria_text .text .criteria_text_3"));
+        // $(".progresss .progress_item .right").after($(".progresss .progress_item .mid"));
     }
     $(".header-menu-mobile .nav_bar_mobile").click(function (){
         $(".modal_menu-mobile").addClass("show-mobile");
